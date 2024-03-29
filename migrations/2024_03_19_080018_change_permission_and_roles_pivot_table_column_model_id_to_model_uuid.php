13f65d0bcefded48a879cb65365955d4e2571901
Ronald A. Richardson <me@ron.dev>
2024-03-29 15:15:52 +0800

    hotfix broken migration, some older fleetbase migrations or legacy version includes `model_id` but newer setups do not

--- migrations/2024_03_19_080018_change_permission_and_roles_pivot_table_column_model_id_to_model_uuid.php
+++ migrations/2024_03_19_080018_change_permission_and_roles_pivot_table_column_model_id_to_model_uuid.php
@@ -4,18 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class() extends Migration {
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('model_has_permissions', function (Blueprint $table) {
-            $table->renameColumn('model_id', 'model_uuid');
+            if (Schema::hasColumn('model_has_permissions', 'model_id')) {
+                $table->renameColumn('model_id', 'model_uuid');
+            }
         });
 
         Schema::table('model_has_roles', function (Blueprint $table) {
-            $table->renameColumn('model_id', 'model_uuid');
+            if (Schema::hasColumn('model_has_roles', 'model_id')) {
+                $table->renameColumn('model_id', 'model_uuid');
+            }
         });
     }
 
@@ -25,11 +29,15 @@ return new class() extends Migration {
     public function down(): void
     {
         Schema::table('model_has_permissions', function (Blueprint $table) {
-            $table->renameColumn('model_uuid', 'model_id');
+            if (Schema::hasColumn('model_has_permissions', 'model_uuid')) {
+                $table->renameColumn('model_uuid', 'model_id');
+            }
         });
 
         Schema::table('model_has_roles', function (Blueprint $table) {
-            $table->renameColumn('model_uuid', 'model_id');
+            if (Schema::hasColumn('model_has_roles', 'model_uuid')) {
+                $table->renameColumn('model_uuid', 'model_id');
+            }
         });
     }
 };
