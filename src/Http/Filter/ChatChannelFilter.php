b603d1efa72c5bb914b1fac3aef6a86e51a3aaef
Doljinsuren Enkhbayar <doljko927@gmail.com>
2024-04-04 16:59:56 +0800

    filter participants

--- src/Http/Filter/ChatChannelFilter.php
+++ src/Http/Filter/ChatChannelFilter.php
@@ -6,7 +6,11 @@ class ChatChannelFilter extends Filter
 {
     public function queryForInternal()
     {
-        $this->builder->where('company_uuid', $this->session->get('company'));
+        $this->builder
+            ->where('company_uuid', $this->session->get('company'))
+            ->whereHas('participants', function ($query) {
+                $query->where('user_uuid', session('user'));
+            });
     }
 
     public function query(?string $query)
