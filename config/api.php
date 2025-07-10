50f1633bcdea8599bba6507326cf10cd236ed2f4
Ronald A. Richardson <me@ron.dev>
2025-07-10 22:26:35 +0800

    v1.6.13 - Configurable throttle

--- config/api.php
+++ config/api.php
@@ -1,3 +1,8 @@
 <?php
 
-return [];
+return [
+    'throttle' => [
+        'max_attempts' => env('THROTTLE_REQUESTS_PER_MINUTE', 90),
+        'decay_minutes' => env('THROTTLE_DECAY_MINUTES', 1),
+    ],
+];
