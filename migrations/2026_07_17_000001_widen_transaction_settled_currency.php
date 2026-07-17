42bae047527c962daa2a3cbbf2a69f35b6cf53ed
Ronald A. Richardson <me@ron.dev>
2026-07-17 16:48:48 +0800

    ran php-cs-fixer

--- migrations/2026_07_17_000001_widen_transaction_settled_currency.php
+++ migrations/2026_07_17_000001_widen_transaction_settled_currency.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Allow non-ISO sandbox and regional currency labels such as KUDOS to be
      * persisted on settled transaction records.
